d55863bd0165c375356ab2c539c7bc21761d995f
FoxxFranc <74159513+FoxxFranc@users.noreply.github.com>
2021-05-08 13:57:02 +0300

    Init

---
+++ Assets/Scripts/Alarm.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Alarm : MonoBehaviour
+{
+    [SerializeField] private float _durationVolume;
+
+    private AudioSource _audio;
+    private float _runningTime, _endingTime, _currentlVolume, _targetVolume;
+    private bool _isWorking;
+
+    private void Start()
+    {
+        _audio = GetComponent<AudioSource>();
+    }
+
+    public void PlaySound()
+    {
+        _audio.Play();
+        _isWorking = true;
+        _targetVolume = 1;
+        _currentlVolume = 0;
+    }
+
+    public void StopSound()
+    {
+        _isWorking = false;
+        _targetVolume = 0;
+        _currentlVolume = 1;
+    }
+
+    private void Update()
+    {      
+        if (_isWorking)
+        {
+            Sound();
+        }
+        else
+        {
+            if(_audio.volume > 0)
+            {
+                Sound();
+                return;
+            } 
+            if (_audio.volume == 0)
+            {
+                _runningTime = 0;
+            }
+        }
+    }
+
+    private void Sound()
+    {
+        _runningTime += Time.deltaTime / _durationVolume;
+        _audio.volume = Mathf.MoveTowards(_currentlVolume, _targetVolume, _runningTime);
+    }
+}
---
+++ Assets/Scripts/EnterRoom.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class EnterRoom : MonoBehaviour
+{
+    [SerializeField] private UnityEvent _entered, _leaved;
+    public event UnityAction Entered
+    {
+        add => _entered?.AddListener(value);
+        remove => _entered?.RemoveListener(value);
+    }
+
+    public event UnityAction Leaved
+    {
+        add => _leaved?.AddListener(value);
+        remove => _leaved?.RemoveListener(value);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.TryGetComponent<Player>(out Player player))
+        {
+            _entered?.Invoke();
+        }
+         
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.TryGetComponent<Player>(out Player player))
+        {
+            _leaved?.Invoke();
+        }
+    }
+}
