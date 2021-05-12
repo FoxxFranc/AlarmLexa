f9cba7002d95cf939fac5b701ce9341c7a281e55
FoxxFranc <74159513+FoxxFranc@users.noreply.github.com>
2021-05-13 02:01:54 +0300

    Init

--- Assets/Scripts/Alarm.cs
+++ Assets/Scripts/Alarm.cs
@@ -8,12 +8,13 @@ public class Alarm : MonoBehaviour
     [SerializeField] private float _durationVolume;
 
     private AudioSource _audio;
-    private float _runningTime, _endingTime, _currentlVolume, _targetVolume;
+    private float _currentlVolume, _targetVolume;
     private bool _isWorking;
 
     private void Start()
     {
         _audio = GetComponent<AudioSource>();
+        StartCoroutine(Ring(_durationVolume));
     }
 
     public void PlaySound()
@@ -31,29 +32,19 @@ public class Alarm : MonoBehaviour
         _currentlVolume = 1;
     }
 
-    private void Update()
-    {      
-        if (_isWorking)
-        {
-            Sound();
-        }
-        else
-        {
-            if(_audio.volume > 0)
-            {
-                Sound();
-                return;
-            } 
-            if (_audio.volume == 0)
-            {
-                _runningTime = 0;
-            }
-        }
-    }
-
-    private void Sound()
+    private IEnumerator Ring(float duration)
     {
-        _runningTime += Time.deltaTime / _durationVolume;
-        _audio.volume = Mathf.MoveTowards(_currentlVolume, _targetVolume, _runningTime);
+        while (true)
+        {
+            if (_isWorking)
+            {
+                _audio.volume += Mathf.MoveTowards(_currentlVolume, _targetVolume, 1/duration/duration);
+            }
+            else
+            {
+                _audio.volume -= Mathf.MoveTowards(_currentlVolume, _targetVolume, 1 - (1/duration/duration));
+            }
+            yield return new WaitForSecondsRealtime(1/duration);
+        }
     }
 }
